fe4ec37a06241306b215d24aa52e136b245db9bf
Lucas Nabesima <lucas.nabesima@gmail.com>
2024-10-28 09:10:09 -0300

    add first challenge statement

--- CsharpExercises/Program.cs
+++ CsharpExercises/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        // Leitura da entrada do usuário
+        string input = Console.ReadLine();
+
+        // Processamento da entrada
+        List<int> anosExperiencia = input.Split(',')
+            .Select(x => int.Parse(x))
+            .ToList();
+
+        // Classificação dos currículos
+        List<string> classificacoes = ClassificarCurriculos(anosExperiencia);
+
+        // Formatação da saída
+        string resultado = string.Join(", ", classificacoes);
+        Console.WriteLine(resultado);
+    }
+
+    static List<string> ClassificarCurriculos(List<int> anosExperiencia)
+    {
+        List<string> classificacoes = new List<string>();
+
+        // TODO: Itere sobre cada número de anos de experiência na lista 'anosExperiencia' e preencha a lista de classificações.
+
+        return classificacoes;
+    }
+}
